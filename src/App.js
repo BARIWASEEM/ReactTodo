37012dbc102b1119d58e6eab292db0a49d201fc7
Mohammed abdul bari waseem <mohdabdulbariwaseem@gmai.com>
2025-09-22 21:18:10 -0400

    Reactapp

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     My Todo Application Updated
+      <FirstComponent />
     </div>
   );
 }
+
+
+function FirstComponent() {
+  return (
+    <div className="FirstComponent">
+     My First Component
+    </div>
+  );
+}
+
+
+
+
+
+
 
 export default App;
