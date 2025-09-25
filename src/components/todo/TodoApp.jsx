8b4b423c56c6bdf3b356a1b1b917ee3b8707b3dc
Mohammed abdul bari waseem <mohdabdulbariwaseem@gmai.com>
2025-09-24 22:38:27 -0400

    react routes navigate and params

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";    
+import {BrowserRouter, Routes, Route, useNavigate, useParams} from 'react-router-dom'
 
 export default function TodoApp() {
   return (
     <div className="TodoApp">
-      Todo Management Application
-      <LoginComponent />
+      <BrowserRouter>
+        <Routes>
+           <Route path="/" element={<LoginComponent />} />
+          <Route path="/Login" element={<LoginComponent />} />
+          
+             // In your routes
+          <Route path='/welcome/:username' element={<WelcomeComponent />} />
+
+          <Route path="*" element={<ErrorComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+
+
 }
 
 function LoginComponent() {
@@ -14,12 +26,17 @@ function LoginComponent() {
   const [password, setPassword] = useState('dummy');
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+const navigate = useNavigate();
+
+
 
   function handleSubmit() {
     if (username === 'in28minutes' && password === 'dummy') {
       console.log('Successful');
       setShowSuccessMessage(true);
       setShowErrorMessage(false);
+      navigate(`/welcome/${username}`); 
+
     } else {
       console.log('Failed');
       setShowErrorMessage(true);
@@ -61,4 +78,16 @@ function LoginComponent() {
       </div>
     </div>
   );
+}
+
+
+function WelcomeComponent() {
+  const {username} = useParams();
+  console.log(username);
+  return <h1>Welcome {username}!</h1>;
+}
+
+
+function ErrorComponent() {
+  return <div>An Error Occurred. I don't know what to do! Contact support at ***-***</div>;
 }
